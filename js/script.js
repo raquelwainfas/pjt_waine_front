4386410fa2594bfcc803efaecf553a9fb3c739a7
Raquel Wainfas <38896841+raquelwainfas@users.noreply.github.com>
2024-09-26 23:04:02 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,117 +1,277 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const toggleFormButton = document.getElementById('toggle-form');
-    const toggleTableButton = document.getElementById('toggle-table');
-    const showTable = document.getElementById('show-table');
-    const inserirVinhoModal = new bootstrap.Modal(document.getElementById('inserirVinhoModal'));
-
-    toggleFormButton.addEventListener('click', function() {
-        inserirVinhoModal.show();
-    });
-
-    const createForm = document.getElementById('create-form');
-    createForm.addEventListener('submit', async function(event) {
-        event.preventDefault();
-        await createVinho();
-        inserirVinhoModal.hide(); // Fecha o modal após enviar o formulário
-        location.reload();
-    });
-
-    toggleTableButton.addEventListener('click', function() {
-        showTable.style.display = (showTable.style.display === 'none' || showTable.style.display === '') ? 'block' : 'none';
-        fetchVinhos();
-    });
-
-    async function fetchVinhos() {
-        try {
-            const response = await fetch('http://localhost:5000/vinhos');
-            if (!response.ok) {
-                throw new Error('Erro ao buscar lista de vinhos qualficados');
-            }
-            const vinhos = await response.json();
-            const tableBody = document.querySelector('#vinhos-table tbody');
-            tableBody.innerHTML = '';
-            vinhos.forEach(vinho => {
-                const row = document.createElement('tr');
-
-                row.innerHTML = `
-                    <td class="align-middle"><img src="${vinho.image_url}" alt="Wine Image" class="img-fluid"></td>
-                    <td class="align-middle">${vinho.id}</td>
-                    <td class="align-middle">${vinho.name}</td>
-                    <td class="align-middle">${vinho.fixed_acidity}</td>
-                    <td class="align-middle">${vinho.volatile_acidity}</td>
-                    <td class="align-middle">${vinho.citric_acid}</td>
-                    <td class="align-middle">${vinho.residual_sugar}</td>
-                    <td class="align-middle">${vinho.chlorides}</td>
-                    <td class="align-middle">${vinho.free_sulfur_dioxide}</td>
-                    <td class="align-middle">${vinho.total_sulfur_dioxide}</td>
-                    <td class="align-middle">${vinho.density}</td>
-                    <td class="align-middle">${vinho.pH}</td>
-                    <td class="align-middle">${vinho.sulphates}</td>
-                    <td class="align-middle">${vinho.alcohol}</td>
-                    <td class="align-middle">${vinho.quality}</td>
-                    <td class="align-middle">
-                        <button class="btn btn-danger" onclick="deleteItem(${vinho.id})">Deletar</button>
-                    </td>
-                `;
-                tableBody.appendChild(row);
-            });
-        } catch (error) {
-            console.error(error);
-            alert('Erro ao listar vinhos');
-        }
-    }
-
-    async function createVinho() {
-        try {
-
-            const name = document.getElementById('create-name').value;
-            const acidez_fixa = document.getElementById("create-acidez-fixa").value;
-            const acidez_volatil = document.getElementById("create-acidez-volatil").value;
-            const acido_citrico = document.getElementById("create-acido-citrico").value;
-            const acucar_residual = document.getElementById("create-acucar-residual").value;
-            const cloretos = document.getElementById("create-cloretos").value;
-            const dioxido_livre = document.getElementById("create-dioxido-livre").value;
-            const dioxido_total = document.getElementById("create-dioxido-total").value;
-            const densidade = document.getElementById("create-densidade").value;
-            const ph = document.getElementById("create-ph").value;
-            const sulfatos = document.getElementById("create-sulfatos").value;
-            const alcool = document.getElementById("create-alcool").value
-
-
-            const response = await fetch('http://localhost:5000/vinho', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ name, acidez_fixa, acidez_volatil, acido_citrico, acucar_residual, cloretos, dioxido_livre, dioxido_total, densidade, ph, sulfatos, alcool })
-            });
-
-            if (!response.ok) {
-                throw new Error('Erro ao qualificar vinho');
-            }
-
-            alert('Vinho cadastrado e qualificado com sucesso!');
-            fetchVinhos();
-            createForm.reset();
-        } catch (error) {
-            console.error(error);
-            alert('Erro ao qualificar/cadastrar vinho');
-        }
-    }
-
-    window.deleteItem = async function(id) {
-        try {
-            const response = await fetch(`http://localhost:5000/vinho/${id}`, { method: 'DELETE' });
-            if (!response.ok) {
-                throw new Error('Erro ao deletar vinho');
-            }
-            alert('Vinho deletado com sucesso!');
-            fetchVinhos();
-        } catch (error) {
-            console.error(error);
-            alert('Erro ao deletar vinho');
-        }
-    }
-
-    fetchVinhos();
+$(document).ready(function () {
+    getList();
 });
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para obter a lista existente do servidor via requisição GET
+  --------------------------------------------------------------------------------------
+*/
+const getList = async () => {
+    let url = 'http://127.0.0.1:5000/vinhos';
+    try {
+        let response = await fetch(url, { method: 'GET' });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        let data = await response.json();
+
+        if (data && data.vinhos && Array.isArray(data.vinhos.vinhos)) {
+            const table = document.getElementById('recordsTable');
+            const tbody = table.querySelector('tbody');
+            tbody.innerHTML = ''; // Limpar apenas o corpo da tabela
+
+            data.vinhos.vinhos.forEach(item => {
+                console.log(item.name);
+                console.log(item.volatile_acidity);
+
+                // if (item.name && item.acidez_fixa && item.acidez_volatil && item.acido_citrico && item.acucar_residual && item.cloretos && item.dioxido_livre && item.dioxido_total && item.densidade && item.ph && item.sulfatos && item.alcool) {
+                if (Object.values(item).every(valor => valor !== null)) {
+                    insertList(
+                        item.name,
+                        item.fixed_acidity,
+                        item.volatile_acidity,
+                        item.citric_acid,
+                        item.residual_sugar,
+                        item.chlorides,
+                        item.free_sulfur_dioxide,
+                        item.total_sulfur_dioxide,
+                        item.density,
+                        item.ph,
+                        item.sulphates,
+                        item.alcohol,
+                        `⭐${"⭐".repeat(item.outcome)} (${item.outcome})`
+
+                    );
+                } else {
+                    console.warn('Dados incompletos:', item);
+                }
+            });
+        } else {
+            console.warn('Nenhum dado encontrado ou formato inválido:', data);
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+};
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para colocar um item na lista do servidor via requisição POST
+  --------------------------------------------------------------------------------------
+*/
+const postItem = async (name, fixed_acidity, volatile_acidity, citric_acid, residual_sugar, chlorides, free_sulfur_dioxide, total_sulfur_dioxide, density, ph, sulphates, alcohol) => {
+    const formData = new FormData();
+    formData.append('name', name);
+    formData.append('fixed_acidity', fixed_acidity);
+    formData.append('volatile_acidity', volatile_acidity);
+    formData.append('citric_acid', citric_acid);
+    formData.append('residual_sugar', residual_sugar);
+    formData.append('chlorides', chlorides);
+    formData.append('free_sulfur_dioxide', free_sulfur_dioxide);
+    formData.append('total_sulfur_dioxide', total_sulfur_dioxide);
+    formData.append('density', density);
+    formData.append('ph', ph);
+    formData.append('sulphates', sulphates);
+    formData.append('alcohol', alcohol);
+
+    const url = 'http://127.0.0.1:5000/vinho';
+
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            body: formData
+        });
+
+        if (!response.ok) {
+            throw new Error(`Erro na requisição: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Erro:', error);
+        return null;
+    }
+};
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para criar um botão de exclusão para cada item da lista
+  --------------------------------------------------------------------------------------
+*/
+const insertDeleteButton = (parent) => {
+    let icon = document.createElement("i");
+    icon.className = "fa fa-trash close";
+    icon.onclick = async function () {
+        let row = this.closest("tr");
+        const nomeItem = row.getElementsByTagName('td')[0].textContent;
+        const result = await Swal.fire({
+            title: 'Você tem certeza?',
+            text: "Este item será removido.",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Sim, remover!',
+            cancelButtonText: 'Cancelar'
+        });
+
+        if (result.isConfirmed) {
+            await deleteItem(nomeItem);
+            row.remove();
+            Swal.fire(
+                'Removido!',
+                'O item foi removido com sucesso.',
+                'success'
+            );
+        }
+    };
+    parent.appendChild(icon);
+}
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para deletar um item da lista do servidor via requisição DELETE
+  --------------------------------------------------------------------------------------
+*/
+const deleteItem = async (item) => {
+    let url = `http://127.0.0.1:5000/vinho?name=${encodeURIComponent(item)}`;
+    try {
+        const response = await fetch(url, {
+            method: 'DELETE'
+        });
+
+        if (!response.ok) {
+            throw new Error(`Erro na requisição: ${response.statusText}`);
+        }
+
+        await response.json();
+        await getList();
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para adicionar um novo item com nome e características
+  --------------------------------------------------------------------------------------
+*/
+const newItem = async () => {
+
+    let inputName = document.getElementById("name").value;
+    let inputAcidezFixa = document.getElementById("fixed_acidity").value;
+    let inputAcidezVolatil = document.getElementById("volatile_acidity").value;
+    let inputAcidoCitrico = document.getElementById("citric_acid").value;
+    let inputAcucarResidual = document.getElementById("residual_sugar").value;
+    let inputCloretos = document.getElementById("chlorides").value;
+    let inputDioxidoLivre = document.getElementById("free_sulfur_dioxide").value;
+    let inputDioxidoTotal = document.getElementById("total_sulfur_dioxide").value;
+    let inputDensidade = document.getElementById("density").value;
+    let inputPh = document.getElementById("ph").value;
+    let inputSulfatos = document.getElementById("sulphates").value;
+    let inputAlcool = document.getElementById("alcohol").value;
+
+    if (inputName === '') {
+        Swal.fire({
+            icon: 'error',
+            title: 'Erro!',
+            text: 'O nome do vinho não pode ser vazio!',
+        });
+        return;
+    }
+
+    if (isNaN(inputAcidezFixa) || isNaN(inputAcidezVolatil) || isNaN(inputAcidoCitrico) || isNaN(inputAcucarResidual) || isNaN(inputCloretos) || isNaN(inputDioxidoLivre) || isNaN(inputDioxidoTotal) || isNaN(inputDensidade) || isNaN(inputPh) || isNaN(inputSulfatos) || isNaN(inputAlcool)) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Erro!',
+            text: 'Os campos precisam ser números!',
+        });
+        return;
+    }
+
+    const checkUrl = `http://127.0.0.1:5000/vinhos?nome=${encodeURIComponent(inputName)}`;
+    try {
+        const response = await fetch(checkUrl, { method: 'GET' });
+        const data = await response.json();
+
+        if (Array.isArray(data.vinhos) && data.vinhos.some(item => item.name === inputName)) {
+            Swal.fire({
+                icon: 'warning',
+                title: 'Vinho já cadastrado',
+                text: 'Cadastre o vinho com um nome diferente ou atualize o existente.',
+            });
+        } else {
+            const result = await Swal.fire({
+                title: 'Confirmação',
+                text: "Você deseja adicionar este item?",
+                icon: 'question',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Sim, adicionar!',
+                cancelButtonText: 'Cancelar'
+            });
+
+            if (result.isConfirmed) {
+                await postItem(inputName, inputAcidezFixa, inputAcidezVolatil, inputAcidoCitrico, inputAcucarResidual, inputCloretos, inputDioxidoLivre, inputDioxidoTotal, inputDensidade, inputPh, inputSulfatos, inputAlcool);
+                Swal.fire(
+                    'Adicionado!',
+                    'O item foi adicionado com sucesso!',
+                    'success'
+                );
+
+                await getList();
+            }
+        }
+    } catch (error) {
+        console.error('Erro:', error);
+        Swal.fire({
+            icon: 'error',
+            title: 'Erro!',
+            text: 'Ocorreu um erro ao verificar o vinho. Por favor, tente novamente.',
+        });
+    }
+};
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para inserir itens na lista apresentada
+  --------------------------------------------------------------------------------------
+*/
+const insertList = (nameVinho, acidezFixa, acidezVolatil, acidoCitrico, acucarResidual, cloretos, dioxidoLivre, dioxidoTotal, densidade, ph, sulfatos, alcool, outcome) => {
+    const table = document.getElementById('recordsTable');
+    const tbody = table.querySelector('tbody'); // Selecione o corpo da tabela
+    const row = tbody.insertRow();
+    console.log("outcome", outcome)
+    const item = [
+        nameVinho,
+        acidezFixa,
+        acidezVolatil,
+        acidoCitrico,
+        acucarResidual,
+        cloretos,
+        dioxidoLivre,
+        dioxidoTotal,
+        densidade,
+        ph,
+        sulfatos,
+        alcool,
+        outcome
+    ];
+
+    item.forEach((value, index) => {
+        const cell = row.insertCell(index);
+        cell.textContent = value;
+    });
+
+    const deleteCell = row.insertCell(item.length);
+    deleteCell.className = "table-action";
+    insertDeleteButton(deleteCell);
+
+    document.getElementById("predictionForm").reset();
+}
+
